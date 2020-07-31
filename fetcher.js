b8f17d03f957d1be0859513db04014c398302aa5
Doug Dyson <dougdyson@gmail.com>
2020-07-31 23:49:16 +0000

    about to start on fs, everything working

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,17 @@
 const request = require('request');
 const fs = require('fs');
 
-let args = process.argv;
 
+let args = process.argv;
+args = args.splice(2);
+console.log(args);
 
 // working code example from earlier exercise....
-request('https://www.google.com/fdsafsafsa.html', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+// request('https://www.google.com/fdsafsafsa.html', (error, response, body) => {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
 
 /*
 REQUIREMENTS:
@@ -22,10 +24,22 @@ Downloaded and saved 3261 bytes to ./index.html
 */
 
 // declare variables
+const urlToGet = args[0];
+const localPathToSave = args[1];
 
-// construct the request url
+console.log('urlToGet', urlToGet, 'localPathToSave', localPathToSave);
 
-// request it
+// request url
+request(urlToGet, (error, response, body) => {
+  console.log('REQUEST error:', error); // Print the error if one occurred
+  console.log('REQUEST statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('REQUEST body:', body); // Print the HTML the page.
+});
+  
+  // //MENTOR: do I pass the body?
+  // write(localPathToSave, body, err => {
+  //   console.log('writing ', localPathtoSave, body);
+  // });
 
 // parse out content-length for size
 
