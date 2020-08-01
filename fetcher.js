b386327570d86c0f1840d7251e27be198bc0b38d
Doug Dyson <dougdyson@gmail.com>
2020-08-01 01:24:49 +0000

    had file open and not saved on lst commit. This one is clean

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,15 @@
+/*
+REQUIREMENTS
+============
+- Use the request library to make the HTTP request
+- Use Node's fs module to write the file
+
+SAMPLE INPUT & EXPECTED OUTPUT
+> node fetcher.js http://www.example.edu/ ./index.html
+Downloaded and saved 3261 bytes to ./index.html
+*/
+
+
 const request = require('request');
 
 const fs = require('fs');
@@ -6,56 +18,29 @@ let args = process.argv;
 args = args.splice(2);
 console.log(args);
 
-// working code example from earlier exercise....
-// request('https://www.google.com/fdsafsafsa.html', (error, response, body) => {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
-/*
-REQUIREMENTS:
-- Use the request library to make the HTTP request
-- Use Node's fs module to write the file
-
-> node fetcher.js http://www.example.edu/ ./index.html
-Downloaded and saved 3261 bytes to ./index.html
-
-*/
 
 // declare variables
 const urlToGet = args[0];
 const localPathToSave = args[1];
 
-console.log('urlToGet', urlToGet, 'localPathToSave', localPathToSave);
+//console.log('urlToGet', urlToGet, 'localPathToSave', localPathToSave);
 
-// request url
 request(urlToGet, (error, response, body) => {
   console.log('REQUEST error:', error); // Print the error if one occurred
   console.log('REQUEST statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('REQUEST body:', body); // Print the HTML the page.
 
-  // let fileDescrip;
-
   // HAPPY PATH
-
   fs.writeFile(localPathToSave, body, function(err) {
     //console.log(body);
     if(err) {
         return console.log('err: ', err);
     } else {
-
       const stats = fs.statSync(localPathToSave);
       const fileSizeInBytes = stats["size"];
       return console.log('Downloaded and saved ' + fileSizeInBytes + ' bytes to ' + localPathToSave);
     }
   }); 
 });
-  
 
-// parse out content-length for size
-
-// download it
-
-// print message if successful, throw err if not
 
